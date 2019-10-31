1f8c5590ea420fd4c3248f018a952939b6d9e986
XingYu9902 <850396118@qq.com>
2019-10-31 11:41:15 +0800

    基础题

---
+++ myClass/src/Test/Main.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(4,5);
+        double area = r1.getArea();
+        System.out.println(area);
+    }
+}
